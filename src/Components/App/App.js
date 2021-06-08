61c39eafa8586348a56c084006372333c9a2b333
Karen Garcia <saykaren@gmail.com>
2021-06-08 16:24:55 -0600

    Drug Data and Footer Added in one drug data for now and added footer

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,13 +1,22 @@
 import "./../Styling/App.scss";
 import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
+import Footer from "./Footer";
+import Main from "./Main";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      cacheTime: 1000 * 60 * 60 * 24,
+    },
+  },
+});
 
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="App">Hello</div>
+      <Main />
+      <Footer />
       <ReactQueryDevtools initialIsOpen={false} position={"bottom-right"} />
     </QueryClientProvider>
   );
