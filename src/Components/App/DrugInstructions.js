5e7b9a544929de94b6b061c4eef89c2f0e35e04b
Karen Garcia <saykaren@gmail.com>
2021-06-09 14:17:48 -0600

    Changes details tags for drug details
    Due to the amount of information I thought it would be helpful to put the drug details into details tag so it has the title then users can pick which areas to expand

--- src/Components/App/DrugInstructions.js
+++ src/Components/App/DrugInstructions.js
@@ -3,13 +3,14 @@ import React from "react";
 const DrugInstructions = ({ sectionArray, title }) => {
   return (
     <div className="drugDetails">
-      {sectionArray && sectionArray.map((des, indexDes) => (
-        <div key={indexDes}>
-          {" "}
-          <div className="drugSectionTitle">{title}</div>
-          <div>{des}</div>
-        </div>
-      ))}
+      {sectionArray &&
+        sectionArray.map((des, indexDes) => (
+          <details key={indexDes}>
+            {" "}
+            <summary className="drugSectionTitle">{title}</summary>
+            <p>{des}</p>
+          </details>
+        ))}
     </div>
   );
 };
