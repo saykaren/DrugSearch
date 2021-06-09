c495972c27394fdd2c1deb6a9c30c3fe349b204f
Karen Garcia <saykaren@gmail.com>
2021-06-09 13:30:05 -0600

    Adds ability to search API by drug name
    Activates the sinput field and search button to issue an API call to pull drug data. Pulls drug details into DrugInstructions component to keep code DRY

--- src/Components/App/Main.js
+++ src/Components/App/Main.js
@@ -2,96 +2,109 @@ import React, { useState } from "react";
 import { useQuery } from "react-query";
 import fetchURL from "./../useQuery/fetchUrl";
 import warning from "./../Assets/warning.png";
+import DrugInstructions from "./DrugInstructions";
 
 const Main = () => {
+  // Abreva - another drug to search
+  // Aceon -- not found
+  const [finalSearchTerm, setFinalSearchTerm] = useState("Asprin");
   const [fdaURL, setFdaUrl] = useState(
-    "https://api.fda.gov/drug/label.json?search=openfda.brand_name:%22ASPRIN%22&limit=10"
+    `https://api.fda.gov/drug/label.json?search=openfda.brand_name:`
   );
 
-
-  const drugData = useQuery(["drug", `${fdaURL}`], fetchURL);
+  const drugData = useQuery(
+    ["drug", `${fdaURL}`, `${finalSearchTerm}`],
+    fetchURL
+  );
   const [searchTerm, setSearchTerm] = useState("");
 
-  console.log({ drugData });
   return (
     <div className="main_component">
-      {drugData.status === "error" && <div>Error Fetching Data</div>}
+      {drugData.status === "error" && <div>NO DATA: Error Fetching Data</div>}
       {drugData.status === "Loading" && <div>Loading Data....</div>}
-      {drugData.status === "success" && drugData.data.results && (
+      {drugData.status === "success" && (
         <>
-          <section className="disclaimer">
-            <img src={warning} className="icon" alt="disclaimer" />
-            <a
-              href={drugData.data.meta.terms}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {drugData.data.meta.disclaimer}{" "}
-            </a>{" "}
-            Last Updated: {drugData.data.meta.last_updated}{" "}
-            <img src={warning} className="icon" alt="disclaimer" />
-          </section>
+          {drugData.data.results && (
+            <section className="disclaimer">
+              <img src={warning} className="icon" alt="disclaimer" />
+              <a
+                href={drugData.data.meta.terms}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {drugData.data.meta.disclaimer}{" "}
+              </a>{" "}
+              Last Updated: {drugData.data.meta.last_updated}{" "}
+              <img src={warning} className="icon" alt="disclaimer" />
+            </section>
+          )}
           <section className="search">
-              <input
+            <input
               type="text"
               value={searchTerm}
-              onChange={(e)=> setSearchTerm(e.target.value)}
-              placeholder="Drug"
-              id="drugInputSearch"/>
-              <button>Submit</button>
+              onChange={(e) => setSearchTerm(e.target.value)}
+              placeholder="Prescription Drug"
+              id="drugInputSearch"
+            />
+            <button onClick={() => setFinalSearchTerm(searchTerm)}>
+              SEARCH
+            </button>
           </section>
-          <section>
-            <h2>Results</h2>
+          {drugData.data.results && (
+            <section>
+              <h2>Results: {finalSearchTerm}</h2>
 
-            <div>
-              {drugData.data.results.length > 0 &&
-                drugData.data.results.map((results, indexResults) => (
-                  <span key={indexResults}>
-                                  <div className="drugDetails">
-                      {results.package_label_principal_display_panel.map((des, indexDes) => (
-                        <div key={indexDes}> Description: {des}</div>
-                      ))}
-                    </div>
-                    <div className="drugDetails">
-                      {results.dosage_and_administration.map(
-                        (des, indexDes) => (
-                          <div key={indexDes}> Description: {des}</div>
-                        )
-                      )}
-                    </div>
-                    <div className="drugDetails">
-                      {results.indications_and_usage.map((des, indexDes) => (
-                        <div key={indexDes}> Description: {des}</div>
-                      ))}
-                    </div>
-                    <div className="drugDetails">
-                      {results.geriatric_use.map((des, indexDes) => (
-                        <div key={indexDes}> Geriatric Use: {des}</div>
-                      ))}
-                    </div>
-                    <div className="drugDetails">
-                      {results.pediatric_use.map((des, indexDes) => (
-                        <div key={indexDes}> Pediatric Use: {des}</div>
-                      ))}
-                    </div>
-                    <div className="drugDetails">
-                      {results.information_for_patients.map((des, indexDes) => (
-                        <div key={indexDes}> Patient Info: {des}</div>
-                      ))}
-                    </div>
-                    <div className="drugDetails">
-                      {results.warnings_and_cautions.map((des, indexDes) => (
-                        <div key={indexDes}> Warnings: {des}</div>
-                      ))}
-                    </div>
-                  </span>
-                ))}
-              {/* Description: {drugData.data.results[0].description.length > 0 && drugData.data.results.description.map((des, indexDes) =>{
-                        <span>{des.x}</span>
-                    })} */}
-            </div>
-          </section>
+              <div>
+                {drugData.data.results.length > 0 &&
+                  drugData.data.results.map((results, indexResults) => (
+                    <span key={indexResults}>
+                      <DrugInstructions
+                        sectionArray={
+                          results.package_label_principal_display_panel
+                        }
+                        title="Description"
+                      />
+                      <DrugInstructions
+                        sectionArray={results.dosage_and_administration}
+                        title="Description"
+                      />
+                      <DrugInstructions
+                        sectionArray={results.indications_and_usage}
+                        title="Description"
+                      />
+                      <DrugInstructions
+                        sectionArray={results.geriatric_use}
+                        title="Geriatric Use"
+                      />
+                      <DrugInstructions
+                        sectionArray={results.pediatric_use}
+                        title="Pediatric Use"
+                      />
+                      <DrugInstructions
+                        sectionArray={results.information_for_patients}
+                        title="Patient Info"
+                      />
+
+                      <DrugInstructions
+                        sectionArray={results.warnings_and_cautions}
+                        title="Warnings"
+                      />
+                    </span>
+                  ))}
+              </div>
+            </section>
+          )}
         </>
+      )}
+      {drugData.status === "success" && drugData.data.error && (
+        <section className="main_component">
+          <h2>
+            {searchTerm} {drugData.data.error.code && drugData.data.error.code}{" "}
+          </h2>
+          <div>
+            {drugData.data.error.message && drugData.data.error.message}
+          </div>
+        </section>
       )}
     </div>
   );
